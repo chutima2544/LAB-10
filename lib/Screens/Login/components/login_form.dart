9f9b7ce1e3df41bd459f97c608675f333f0fdf82
chutima <chutimamaa2544@gmail.com>
2022-09-26 05:40:39 +0700

    E2

--- lib/Screens/Login/components/login_form.dart
+++ lib/Screens/Login/components/login_form.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Lobby.dart';
 
@@ -6,9 +8,46 @@ import '../../../constants.dart';
 import '../../Signup/signup_screen.dart';
 
 class LoginForm extends StatelessWidget {
-  const LoginForm({
+   LoginForm({
     Key? key,
   }) : super(key: key);
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  Widget FireLog(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return LoginForm();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }),
+    );
+  }
+
+  static Future<User?> loginUsingEmailPassword({required String email,required String password,required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e){
+      if (e.code == "user-not=foud") {
+        print("No User foud for that email");
+      }
+    }
+    return user;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +55,7 @@ class LoginForm extends StatelessWidget {
       child: Column(
         children: [
           TextFormField(
+            controller: _emailController,
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
@@ -31,6 +71,7 @@ class LoginForm extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
             child: TextFormField(
+              controller: _passwordController,
               textInputAction: TextInputAction.done,
               obscureText: true,
               cursorColor: kPrimaryColor,
@@ -47,12 +88,12 @@ class LoginForm extends StatelessWidget {
           Hero(
             tag: "login_btn",
             child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute( 
-                    builder: (context) => Lobby())
-                    );
+              onPressed: () async {
+                User? user = await loginUsingEmailPassword(email: _emailController.text, password: _passwordController.text, context: context);
+                print(user);
+                if (user != null){
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Lobby()));
+                }
               },
               child: Text(
                 "Login".toUpperCase(),
--- lib/Screens/Signup/components/signup_form.dart
+++ lib/Screens/Signup/components/signup_form.dart
@@ -1,12 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
 
 class SignUpForm extends StatelessWidget {
-  const SignUpForm({
+   SignUpForm({
     Key? key,
   }) : super(key: key);
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  Widget FireLog(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SignUpForm();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +38,7 @@ class SignUpForm extends StatelessWidget {
       child: Column(
         children: [
           TextFormField(
+            controller: _emailController,
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
@@ -29,6 +54,7 @@ class SignUpForm extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
             child: TextFormField(
+              controller: _passwordController,
               textInputAction: TextInputAction.done,
               obscureText: true,
               cursorColor: kPrimaryColor,
